02c29cc7628eeb441a82a8cc886890cd91000b83
Ricardo <soarespoa@gmail.com>
2022-07-09 06:44:54 -0300

    Trying resolve problem deploy Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,11 +21,14 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
+
+  
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
-
+   # In your Rails configuration
+  # config.assets.precompile += ["marketing.css"]
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
